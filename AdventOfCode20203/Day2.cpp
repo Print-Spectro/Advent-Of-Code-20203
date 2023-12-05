8700393192743136c0fdc3dcc137184846f2f23f
Angelo Davis <francisdavia11@gmail.com>
2023-12-05 22:32:12 +0000

    day2part2solved

--- AdventOfCode20203/Day2.cpp
+++ AdventOfCode20203/Day2.cpp
@@ -27,6 +27,24 @@ bool colourcheck(const std::string row, const std::tuple<int, std::string> i, co
 	return 0;
 }
 
+int biggestnumber(const std::string row, const std::tuple<int, std::string> i, const int parse_pos = 0, int biggest = 0)
+{
+	auto pos{ row.find(std::get<1>(i), parse_pos) };
+	if (pos != std::string::npos) {
+		std::string temp{""};
+		temp += row[pos - 3];
+		temp += row[pos - 2];
+		int number_drawn = std::stoi(temp);
+		int testvalue = std::get<0>(i);
+		if (number_drawn > biggest) {
+			biggest = number_drawn;
+		}
+		int(position) = static_cast<int>(pos + 2);
+		return biggestnumber(row, i, position, biggest);
+	}
+	return biggest;
+}
+
 int colourparse(const std::string row) {
 	std::vector<std::tuple<int, std::string>> colours{{12, "red"}, { 13, "green" }, { 14, "blue"}};
 	for (std::tuple<int, std::string> i : colours) {
@@ -37,31 +55,47 @@ int colourparse(const std::string row) {
 	return 1;
 }
 
+int cubepower(const std::string row) {
+	std::vector<std::tuple<int, std::string>> colours{{12, "red"}, { 13, "green" }, { 14, "blue" }};
+	int power[3]{0, 0, 0};
+	int index = 0;
+	for (std::tuple<int, std::string> i : colours) {
+		power[index] = biggestnumber(row, i);
+		index++;
+	}
+	int totalpower = power[0] * power[1] * power[2];
+	return totalpower;
+}
+
+
 int day2() {
 	std::string filename{R"(C:\Users\franc\source\repos\AdventOfCode20203\AdventOfCode20203\Day2Input.txt)"};
 	std::ifstream file{filename};
 	if (!file.is_open()) {
 		return 0;
 	}
-
 	std::string row{};
 	int rowcount{1};
 	int total{0};
+	int totalpower{0};
 	while (std::getline(file, row)) {
-		
-		 if (colourparse(row)) {
-		 total+= rowcount;
-		 std::cout << rowcount << std::endl;
-		 }
-		
+		totalpower += cubepower(row);
+		std::cout <<cubepower(row) << std::endl;
+		if (colourparse(row)) {
+		total+= rowcount;
+		//std::cout << rowcount << std::endl;
+		}
 		rowcount++;
 	}
+	//std::cout << total << std::endl;
+	std::cout << totalpower << std::endl;
 	return total;
+	
 }
 
 
 int main() {
-	std::cout << day2();
+	day2();
 }
 
 
